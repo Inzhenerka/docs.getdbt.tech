e9c9f7eba691d6d8b01468feb964ad5e236f802b
Jason Karlavige <jkarlavige@gmail.com>
2025-03-13 09:52:58 -0400

    remove consoles

--- website/static/js/hide-forethought.js
+++ website/static/js/hide-forethought.js
@@ -1,26 +1,20 @@
 (function() {
-  console.log('hide forethought script running')
   // Check if QualifiedConversationStarted cookie set
   const qualifiedCookie = document.cookie
     .split("; ")
     .find((row) => row.startsWith("QualifiedConversationStarted="))
-  console.log('qualifiedCookie', qualifiedCookie)
 
   // If QualifiedConversationStarted cookie set, 
   // attempt to hide Forethought widget
   let intCount = 0
   if(qualifiedCookie) {
-    console.log('hiding forethought widget')
     // Start interval to allow time for Forethought widget element to load
     const interval = setInterval(function(){
       intCount++
       // Attempt to get forethought-chat widget element 5 times
-      console.log('count', intCount)
       if (intCount < 10) {
-        
         // Get Forethought widget by ID
         const forethoughtIframe = document.getElementById('forethought-chat')
-        console.log('forethoughtIframe', forethoughtIframe)
         if (forethoughtIframe) {
           // If element found, hide element
           forethoughtIframe.style.display = 'none';
