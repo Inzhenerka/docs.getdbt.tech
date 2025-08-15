63b0a9ac58d3e0d140cf23a5df7e4612ffbf14da
Jason Karlavige <jkarlavige@gmail.com>
2025-08-15 11:19:47 -0400

    begin styling buttons

--- website/src/components/feedback/index.js
+++ website/src/components/feedback/index.js
@@ -1,4 +1,5 @@
 import styles from "./styles.module.css";
+import { Link } from "@docusaurus/Link";
 
 /**
  * Feedback component to handle visitor feedback for the current docs page.
@@ -9,6 +10,12 @@ export const Feedback = () => {
   return (
     <div className={styles.feedbackContainer}>
       <h2 className={styles.feedbackHeader}>Was this page helpful?</h2>
+      <form>
+        <div className={styles.feedbackButtons}>
+          <button className={styles.feedbackButton}>Yes</button>
+          <button className={styles.feedbackButton}>No</button>
+        </div>
+      </form>
     </div>
   );
 };
