7ba39c33db6af79fd0c51cb409df81c9064ec2d5
Jason Karlavige <jkarlavige@gmail.com>
2025-08-18 13:07:40 -0400

    update api response message

--- website/src/components/feedback/index.js
+++ website/src/components/feedback/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Link from "@docusaurus/Link";
 import { ThumbsUp } from "./ThumbsUp";
 import { ThumbsDown } from "./ThumbsDown";
@@ -9,21 +10,68 @@ import styles from "./styles.module.css";
  */
 
 export const Feedback = () => {
+  const [submissionStatus, setSubmissionStatus] = useState(null);
+
+  const handleFeedbackSubmit = async (feedbackValue) => {
+    setSubmissionStatus("loading");
+
+    try {
+      const response = await fetch(
+        'http://localhost:3000/api/submit-feedback',
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            is_positive: feedbackValue,
+            page_url: window.location.href,
+          }),
+        }
+      );
+
+      // If error, set submission status to error and throw error
+      if (response.error) {
+        throw new Error(response.error);
+      }
+
+      // If success, set submission status to success
+      setSubmissionStatus("success");
+    } catch (error) {
+      setSubmissionStatus("error");
+    }
+  };
+
   return (
     <div className={styles.feedbackContainer}>
       <h2 className={styles.feedbackHeader}>Was this page helpful?</h2>
-      <form>
-        <div className={styles.feedbackButtons}>
-          <button className={styles.feedbackButton}>
-            <ThumbsUp />
-            Yes
-          </button>
-          <button className={styles.feedbackButton}>
-            <ThumbsDown />
-            No
-          </button>
-        </div>
-      </form>
+      <div className={styles.feedbackButtons}>
+        <button 
+          className={styles.feedbackButton}
+          onClick={() => handleFeedbackSubmit(true)}
+        >
+          <ThumbsUp />
+          Yes
+        </button>
+        <button 
+          className={styles.feedbackButton}
+          onClick={() => handleFeedbackSubmit(false)}
+        >
+          <ThumbsDown />
+          No
+        </button>
+        {submissionStatus === "loading" ? (
+          <span className={styles.feedbackMessage}>
+            Submitting feedback...
+          </span>
+          ) : submissionStatus === "success" ? (
+          <span className={styles.feedbackMessage}>Thank you for your feedback!</span>
+        ) : submissionStatus === "error" ? (
+          <span className={styles.feedbackMessage}>
+            Error submitting feedback. Please try again.
+          </span>
+        ) : null}
+      </div>
       <div className={styles.feedbackLinks}>
         <Link
           href="https://www.getdbt.com/cloud/privacy-policy"
