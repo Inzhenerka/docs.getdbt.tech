dbd68291f63d797fd8f903a6d6cd87c249e73b79
Matt Shaver <60105315+matthewshaver@users.noreply.github.com>
2025-08-27 15:22:27 -0400

    Updating versions

--- website/dbt-versions.js
+++ website/dbt-versions.js
@@ -19,14 +19,18 @@ exports.versions = [
     customDisplay: "dbt Fusion engine",
     isPrerelease: true,
   },
+    {
+    version: "1.11",
+    customDisplay: "Latest (dbt platform only)",
+  },
   {
     version: "1.10",
-    customDisplay: "Latest (dbt Core v1.10)",
+    customDisplay: "Compatible (dbt Core v1.10)",
     EOLDate: "2026-06-15",
   },
   {
     version: "1.9",
-    customDisplay: "Compatible (dbt Core v1.9)",
+    customDisplay: "Extended (dbt Core v1.9)",
     EOLDate: "2025-12-08",
   },
 ];
