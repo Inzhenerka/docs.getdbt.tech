34288b293f5f1d0072b1ba6c228d20be3ac9fc36
Jason Karlavige <jkarlavige@gmail.com>
2025-09-11 16:31:43 -0400

    revert to preview endpoint

--- website/src/components/feedback/index.js
+++ website/src/components/feedback/index.js
@@ -70,9 +70,9 @@ export const Feedback = () => {
 
       // TODO: Change to production URL
       // "https://www.getdbt.com/api/submit-feedback",
-      // "https://www-getdbt-com-git-feedback-input-dbt-labs.vercel.app/api/submit-feedback",
+      // "http://localhost:3000/api/submit-feedback",
       const response = await fetch(
-        "http://localhost:3000/api/submit-feedback",
+        "https://www-getdbt-com-git-feedback-input-dbt-labs.vercel.app/api/submit-feedback",
         {
           method: "POST",
           headers: {
