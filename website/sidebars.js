51691591299eadca295701b6f4ff64f03e814860
mirnawong1 <mirnawong@gmail.com>
2025-10-24 17:14:15 +0100

    add new page

--- website/sidebars.js
+++ website/sidebars.js
@@ -366,6 +366,7 @@ const sidebarSettings = {
             "docs/about-dbt-extension",
             "docs/dbt-extension-features", 
             "docs/install-dbt-extension",
+            "docs/use-dbt-extension",
           ],
         },
         {
